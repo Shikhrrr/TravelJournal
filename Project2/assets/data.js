83d378c87e8482001992894c85d369d158c15bba
shikhrrr <shikhar20396.dpskalyanpur@gmail.com>
2024-03-10 20:08:01 +0530

    added files

---
+++ Project2/assets/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        imageUrl: '../assets/locationPics/mount-fuji.avif',
+        country: 'Japan',
+        mapsLink: 'https://maps.google.com/?q=Mount+Fuji',
+        placeName: 'Mount Fuji',
+        tripDuration: '10 May, 2024 - 15 May, 2024',
+        description: 'Mount Fuji, located on Honshu Island, is the highest peak in Japan, known for its symmetrical cone. A UNESCO World Heritage site, it attracts hikers and climbers year-round.'
+    },
+    {
+        id: 2,
+        imageUrl: '../assets/locationPics/eiffel_tower.jpg',
+        country: 'France',
+        mapsLink: 'https://maps.google.com/?q=Eiffel+Tower',
+        placeName: 'Eiffel Tower',
+        tripDuration: '20 Jun, 2024 - 23 Jun, 2024',
+        description: 'The Eiffel Tower, a wrought-iron lattice tower on the Champ de Mars in Paris, France, is a global cultural icon of France. An engineering marvel, it offers stunning views of the city.'
+    },
+    {
+        id: 3,
+        imageUrl: '../assets/locationPics/colosseum.jpeg',
+        country: 'Italy',
+        mapsLink: 'https://maps.google.com/?q=Colosseum',
+        placeName: 'Colosseum',
+        tripDuration: '15 Jul, 2024 - 18 Jul, 2024',
+        description: 'The Colosseum, also known as the Flavian Amphitheatre, is an ancient Roman amphitheater located in the center of Rome, Italy. It is the largest amphitheater ever built and a UNESCO World Heritage site.'
+    },
+    {
+        id: 4,
+        imageUrl: '../assets/locationPics/gbf.jpeg',
+        country: 'Australia',
+        mapsLink: 'https://maps.google.com/?q=Great+Barrier+Reef',
+        placeName: 'Great Barrier Reef',
+        tripDuration:'10 Aug, 2024 - 17 Aug, 2024',
+        description: 'The Great Barrier Reef, off the coast of Queensland, Australia, is the world\'s largest coral reef system. It is home to an incredible diversity of marine life and is a UNESCO World Heritage site.'
+    }
+];
